c00dfee22e2a1f963604ede48dcccc1d64111a49
Sandy Yeung <sandynism@gmail.com>
2017-09-11 23:51:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {artistName: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign(playlist, {artistName: songTitle})
-  return playlist
+  return playlist;
 }
